f0433c8e3c761aab609adc9800d930b4ac840b06
dragosbanea <154266885+dragosbanea@users.noreply.github.com>
2025-05-24 15:42:24 +0300

    Logging backend

--- src/main/java/project/fmihub/backend/Service/AttendanceService.java
+++ src/main/java/project/fmihub/backend/Service/AttendanceService.java
@@ -3,19 +3,26 @@ package project.fmihub.backend.Service;
 
 import org.springframework.stereotype.Service;
 import project.fmihub.backend.Client.AttendanceClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Set;
 
 @Service
 public class AttendanceService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AttendanceService.class);
     private final AttendanceClient client;
 
     public AttendanceService(AttendanceClient client) {
         this.client = client;
+        logger.info("AttendanceService initialized with AttendanceClient");
     }
 
     public Set<String> printUniqueCourses(String uid) {
+        logger.info("Fetching unique courses for user id: {}", uid);
         String url = "https://www.cs.ubbcluj.ro/apps/orar/api/user/classes/" +uid + "/ro-RO";
+        logger.debug("Constructed URL: {}", url);
         return client.fetchUniqueCourseNames(url);
     }
 }
